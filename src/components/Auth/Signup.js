e93c0e298544922efe28e45420ddc63e8eb4a2c5
Jerome Flaherty <jayromulus.f@gmail.com>
2020-02-17 19:18:49 -0500

    fixed login signup

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import './Signup.css'
 import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import APIURL from '../../helpers/environment';
 
 
 function Signup(props) {
@@ -11,7 +12,7 @@ function Signup(props) {
 
     let handleSubmit = (event) => {
         event.preventDefault()
-        fetch("http://localhost:4000/api/user/createuser", {
+        fetch(`${APIURL}/api/user/createuser`, {
             method: 'POST',
             body: JSON.stringify({ user: { username: username, password: password} }),
             headers: new Headers({
